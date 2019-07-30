8bc3b3fdd558d73719c58ea8e91775b2d159edbc
kobus <kobus@cs.arizona.edu>
2019-07-30 14:31:11 -0700

    Testing with the rebound idea

--- kjb/lib/n2/n2_gen.h
+++ kjb/lib/n2/n2_gen.h
@@ -18,6 +18,7 @@
 |
 * =========================================================================== */
 
+/* Testing repo_sync */
 /* This file should only include other files which are protected for C++ use. */
 
 #ifdef WAS_MAKEDEPEND /* Now obsolete. */
