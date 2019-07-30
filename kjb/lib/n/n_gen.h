168712231ef002651647bf181f206c7726ea6864
kobus <kobus@cs.arizona.edu>
2019-07-30 13:50:07 -0700

    Testing similar to what Adarsh might have tried

--- kjb/lib/n/n_gen.h
+++ kjb/lib/n/n_gen.h
@@ -18,6 +18,8 @@
 |
 * =========================================================================== */
 
+/* TEST repo sync */
+
 /* This file should only include other files which are protected for C++ use. */
 
 #ifdef WAS_MAKEDEPEND /* Now obsolete. */
