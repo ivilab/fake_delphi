75b8a0124d45ff6f640883c61bfc46f2166cf587
ivilab-sync <sync@ivilab.org>
2019-12-13 16:02:44 -0700

    r24706 | kobus | 2019-12-13 16:00:26 -0700 (Fri, 13 Dec 2019) | 1 line
    Lazy commit
    
    Recorded result of commands processed by svn in /home/ivilab/repo_sync/ivilab_fake_delphi_local/src/lib:
    Updating '.':
    U    l/l_io.c
    Updated to revision 24706.

--- kjb/lib/l/l_io.c
+++ kjb/lib/l/l_io.c
@@ -1,5 +1,5 @@
 
-/* $Id: l_io.c 24705 2019-12-13 22:59:25Z kobus $ */
+/* $Id: l_io.c 24706 2019-12-13 23:00:26Z kobus $ */
 
 /* =========================================================================== *
 |
@@ -2859,7 +2859,7 @@ int push_no_overwrite(int no_overwrite)
 {
     int* save_no_overwrite_ptr; 
     int result = NO_ERROR;
-#ifdef TRACK_MEMORY_ALLOCATION
+#ifdef MUST_CLEANUP
     static int  fs_first_no_overwrite_stack_use = TRUE; 
 #endif 
 
